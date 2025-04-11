010f4fc8998198cb565edec294428cd5bf78b45e
Cai-Roger <roger.cai@hand-global.com>
2025-04-11 12:31:11 +0800

    Update mainV2.1.py
    寄件人名稱修改

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def send_mail(req: MailRequest):
         print("📨 收到寄信請求:", req.dict())
 
         msg = EmailMessage()
-        msg["From"] = EMAIL_USER
+        msg["From"] = 飛書系統管理員
         msg["To"] = req.to
         msg["Bcc"] = req.bcc
         msg["Subject"] = req.subject
